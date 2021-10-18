12966c13d8dd1e64ef6ee28bc4b62fe3a0fdf1f8
DatoneX <cgomwe@ymail.com>
2021-10-18 21:26:43 +0100

    Adding tasks to database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
   // sidenav initialization
   $('.sidenav').sidenav();
+  // datepicker initialization
+  $('.datepicker').datepicker({
+    format: 'dd mmm yyyy',
+    i18n: { done: 'Select' },
+  });
+  // select initialization
+  $('select').formSelect();
 });
