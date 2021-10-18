2748e07e29977da721dcc2af920d645908b82321
DatoneX <cgomwe@ymail.com>
2021-10-18 18:14:46 +0100

    Adding categories to database

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -6,7 +6,7 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship(
-        "Task", backref="category", cascade="all, delete", lazy="True"
+        "Task", backref="category", cascade="all, delete", lazy=True
     )
 
     def __repr__(self):
